a5a43d052952f7e59730e0c1d2ef8ad3bace66e3
tonychiu1026 <70912600+tonychiu1026@users.noreply.github.com>
2021-11-01 21:18:42 -0400

    Upload

---
+++ Assets/starting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starting : MonoBehaviour 
+{
+	string[] args = new string[10];
+    string username = "username";
+    string password = "password";
+
+	void Start ()
+	{
+		args = System.Environment.GetCommandLineArgs();
+        Debug.Log(args[2]);
+	}
+	
+	void OnGUI()
+	{
+        /*
+        for(int i = 0; i < args.Length; i++)
+		    GUILayout.Label(args[i]);
+        */
+        GUILayout.Label(args[3]);
+        GUILayout.Label(args[6]);
+
+	}
+
+    void Update()
+    {
+        if (args[3] != username || args[6] != password)
+        {
+            Application.Quit();
+        }
+    }
+}
